7043065e03d0cd7da645c0a8865d1f94f25c8463
modelorona <angel.hristozov@gmail.com>
2017-12-13 00:26:12 +0000

    Initial commit

---
+++ downloader.py
@@ -0,0 +1,26 @@
+import requests
+import sys
+from specific_file_downloader import get_contents
+from bs4 import BeautifulSoup
+
+link = "https://github.com/adambard/learnxinyminutes-docs"
+gh = "https://github.com"
+# raw_link = "https://raw.githubusercontent.com/adambard/learnxinyminutes-docs/master"
+# /awk.html.markdown"
+
+page = requests.get(link)
+page = BeautifulSoup(page.text, "html.parser")
+
+all_links = [link['href'] for link in page.find_all('a', href=True)]
+
+markdown_links = {}
+
+for link in all_links:
+    if str(link).endswith('.html.markdown'):
+        full_link = gh + link
+        file_contents = get_contents(full_link)
+        print(file_contents)
+        print(sys.getsizeof(file_contents))
+        break
+
+# print(markdown_links)
---
+++ specific_file_downloader.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_contents(location):
+    page = requests.get(location).text
+    soup = BeautifulSoup(page, "html.parser")
+    links_in_page = [link['href'] for link in soup.find_all('a', href=True)] # get all the links
+    # now need the raw link, which has raw in the address
+    for link in links_in_page:
+        if "raw" in link:
+            return requests.get("https://github.com" + link).text
+
