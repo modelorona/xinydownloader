83ba60a8d353c8c446480925553dc79b52425777
Anguel <angel.hristozov@gmail.com>
2018-07-01 09:50:30 +0100

    fixed bug where c++ was being shown properly

--- firestore/GithubHelper.py
+++ firestore/GithubHelper.py
@@ -21,6 +21,9 @@ class GithubHelper:
         all_links = [link['href'] for link in data_soup.find_all('a', href=True)]
         # now that we have all the links, time to find the markdown ones
         for link in [l for l in all_links if str(l).endswith('.html.markdown')]:
-            language_name = unescape(str(link).split('/')[::-1][0].split('.')[0])
+            language_name = unescape(str(link).split('/')[::-1][0].split('.')[0]).replace('%2B', '+')
             md_links[language_name] = link
         return md_links if len(md_links) > 0 else None
+
+    def html_to_uni(self, text):
+        return
